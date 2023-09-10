5ee678fbfdec7a6883c46ddec17f7435f3f6630d
Volodymyr Reka <41143080+VovaReka23@users.noreply.github.com>
2023-09-10 18:34:27 +0300

    add hover

--- js/script.js
+++ js/script.js
@@ -8,9 +8,24 @@ window.onload = (event) => {
     toggle.onclick = toggleReadMore;
 
     function toggleReadMore() {
-        console.log('test')
         more.classList.toggle("hidden-zoot");
-        toggle.innerText = isExpanded ? "Learn less" : "Learn more"
+        toggle.innerText = isExpanded ? "See less" : "Learn more"
         isExpanded = !isExpanded;
     }
+
+
+    let tabs = document.querySelectorAll(".tab-zoot");
+
+    for (i = 0; i < tabs.length; i++) {
+		tabs[i].addEventListener("click", tabActive)
+	}
+
+    function tabActive() {
+        for (i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove("active");
+        }
+        this.classList.add("active");
+    }
+
 };
+
